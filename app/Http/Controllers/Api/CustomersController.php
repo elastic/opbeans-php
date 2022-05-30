9a899b69eccdd525cf426e9ec3391f801941d1f8
Ivan Tymkul <ivan_tymkul@epam.com>
2022-05-30 12:21:36 +0300

    Update migrations (add keys). Add relations to models

--- app/Http/Controllers/Api/CustomersController.php
+++ app/Http/Controllers/Api/CustomersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Resources\StatsResource;
 use App\Models\Customers;
+use App\Models\OrderLines;
 use App\Models\Orders;
 use App\Models\Products;
 
@@ -13,9 +14,15 @@ class CustomersController extends Controller
     public function stats()
     {
         return (new StatsResource(collect([
-            'products_count' => Products::count(),
-            'customers_count' => Customers::count(),
-            'orders_count' => Orders::count()
+            'products' => Products::all(),
+            'customers' => Customers::all(),
+            'orders' => Orders::all(),
+            'orderLines' => OrderLines::all(),
         ])))->jsonSerialize();
+    }
+
+    protected function customers()
+    {
+        return Customers::all()->take(100);
     }
 }
