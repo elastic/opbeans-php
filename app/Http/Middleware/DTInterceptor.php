22cff7497785bd04bf405c1d9a9c0ab686b84a3c
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2022-07-06 18:29:05 +0300

    Added tests and instructions to README for distributed tracing

--- app/Http/Middleware/DTInterceptor.php
+++ app/Http/Middleware/DTInterceptor.php
@@ -5,27 +5,138 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class DTInterceptor
 {
-    public function handle(Request $request, Closure $next)
+    private const OPBEANS_DT_PROBABILITY_ENV_VAR_NAME = 'OPBEANS_DT_PROBABILITY';
+    private const OPBEANS_DT_PROBABILITY_DEFAULT = 0.5;
+
+    private const OPBEANS_SERVICES_ENV_VAR_NAME = 'OPBEANS_SERVICES';
+
+    private bool $isEnabled = false;
+
+    private float $probability;
+
+    /** @var string[] */
+    private array $services;
+
+    public function __construct()
     {
-        $hostList = [];
-        $services = explode(',', env('OPBEANS_SERVICES'));
-        $probability = floatval(env("OPBEANS_DT_PROBABILITY", "0.5"));
+        $buildForwardingWillDisabledMsg = function (string $reason): string {
+            return $reason . ' - forwarding to other backend services will be disabled';
+        };
 
-        foreach ($services as $service){
-            if (empty($service) || str_contains($request->path(), 'stats')) {
-                return $next($request);
-            }
-
-            $hostList[] = 'http://' . $service . ':3000/' . $request->path();
+        $probabilityEnvVarVal = env(self::OPBEANS_DT_PROBABILITY_ENV_VAR_NAME);
+        if ($probabilityEnvVarVal === null) {
+            Log::info(
+                $buildForwardingWillDisabledMsg(
+                    self::OPBEANS_DT_PROBABILITY_ENV_VAR_NAME . ' environment variable is not set'
+                ),
+                ['this' => $this->selfToLog()]
+            );
+            return;
         }
 
-        if( (mt_rand() / mt_getrandmax()) <= $probability){
-            $response = Http::get($hostList[array_rand($hostList)])->json();
+        if (filter_var($probabilityEnvVarVal, FILTER_VALIDATE_FLOAT) === false) {
+            $probability = self::OPBEANS_DT_PROBABILITY_DEFAULT;
+            Log::error(
+                self::OPBEANS_DT_PROBABILITY_ENV_VAR_NAME . ' environment variable value'
+                . ' (' . $probabilityEnvVarVal. ')'
+                . ' is not a valid float - using default value (' . self::OPBEANS_DT_PROBABILITY_DEFAULT . ')'
+            );
+        } else {
+            $probability = floatval($probabilityEnvVarVal);
+        }
 
-            abort(response()->json($response));
+        $servicesEnvVarVal = env(self::OPBEANS_SERVICES_ENV_VAR_NAME);
+        if ($servicesEnvVarVal === null) {
+            Log::info(
+                $buildForwardingWillDisabledMsg(
+                    self::OPBEANS_SERVICES_ENV_VAR_NAME . ' environment variable is not set'
+                ),
+                ['this' => $this->selfToLog()]
+            );
+            return;
+        }
+
+        $services = self::parseServices($servicesEnvVarVal);
+        if (empty($services)) {
+            Log::error(
+                $buildForwardingWillDisabledMsg(
+                    self::OPBEANS_SERVICES_ENV_VAR_NAME . ' environment variable does not contain valid URLs'
+                ),
+                ['this' => $this->selfToLog()]
+            );
+            return;
+        }
+
+        $this->probability = $probability;
+        $this->services = $services;
+        $this->isEnabled = true;
+        Log::info('Forwarding to other backend services is enabled', ['this' => $this->selfToLog()]);
+    }
+
+    /**
+     * @return string[]
+     */
+    private static function parseServices(string $envVarVal): array
+    {
+        $result = [];
+        $envVarValAsList = explode(',', $envVarVal);
+        foreach ($envVarValAsList as $urlPrefix){
+            $urlPrefix = trim($urlPrefix);
+            if (empty($urlPrefix)) {
+                continue;
+            }
+            if (str_ends_with($urlPrefix, '/')) {
+                $urlPrefix = substr($urlPrefix, 0, -1);
+            }
+
+            $result[] = $urlPrefix;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    private function selfToLog(): array
+    {
+        return get_object_vars($this);
+    }
+
+    private function randomDecideIfToForward(): bool
+    {
+        return (mt_rand() / mt_getrandmax()) <= $this->probability;
+    }
+
+    private function buildForwardUrl(string $service, string $requestPath): string
+    {
+        return str_starts_with($requestPath, '/') ? ($service . $requestPath) : ($service . '/' . $requestPath);
+    }
+
+    private function randomForwardToService(Request $request): bool
+    {
+        if (!$this->randomDecideIfToForward()) {
+            Log::info('Decided not to forward', ['this' => $this->selfToLog()]);
+            return false;
+        }
+
+        $serviceIndex = array_rand($this->services);
+        $forwardUrl = $this->buildForwardUrl($this->services[$serviceIndex], $request->path());
+        Log::info('Decided to forward', ['forwardUrl' => $forwardUrl, 'this' => $this->selfToLog()]);
+
+        $response = Http::get( $forwardUrl)->json();
+        abort(response()->json($response));
+    }
+
+    public function handle(Request $request, Closure $next)
+    {
+        if ($this->isEnabled && $this->randomForwardToService($request)) {
+            // this return is not reachable
+            return null;
         }
 
         return $next($request);
--- tests/tests.bats
+++ tests/tests.bats
@@ -5,66 +5,71 @@ load 'test_helper/bats-assert/load'
 load test_helpers
 
 IMAGE="bats-opbeans"
-OPBEANS_PHP_APP_CONTAINER_NAME="opbeans-php-app"
-OPBEANS_PHP_WEB_CONTAINER_NAME="opbeans-php-web"
+OPBEANS_PHP_CONTAINER_NAME="opbeans-php"
+OPBEANS_PHP_FRONTEND_CONTAINER_NAME="opbeans-php-frontend"
 DOCKER_COMPOSE_WITH_POSTGRESQL_CMD_PREFIX="docker-compose --env-file docker-compose_env_for_PostgreSQL.txt"
 
-@test "Arrange - Build docker images" {
-	cd $BATS_TEST_DIRNAME/..
-	run docker-compose build
+runCommand() {
+    local -r command_to_run="$@"
+    run echo "Running command: ${command_to_run} ..."
+    run ${command_to_run}
 	assert_success
+    run echo "Output: ${output}"
+    run echo "${output}"
 }
 
-@test "Act - Start docker containers" {
-	run docker-compose up -d
-	assert_success
-}
-
-@test "Assert that docker containers are running" {
-	run docker inspect -f {{.State.Running}} $OPBEANS_PHP_APP_CONTAINER_NAME
-	run docker inspect -f {{.State.Running}} $OPBEANS_PHP_WEB_CONTAINER_NAME
-	assert_output --partial 'true'
-}
-
-@test "Assert that opbeans app is running at port ${PORT}" {
-	sleep 50
-	URL="http://127.0.0.1:${PORT}"
-	run curl -v --fail --connect-timeout 10 --max-time 30 "${URL}/"
-	assert_success
+assertRunningWebServer() {
+    local -r web_server_desc="$1"
+    local -r web_server_url="$2"
+    run echo "Assert that ${web_server_desc} responds at ${web_server_url}"
+	runCommand curl -v --fail --connect-timeout 10 --max-time 30 "${web_server_url}"
 	assert_output --partial 'HTTP/1.1 200'
 }
 
-@test "Tear down - Stop docker containers" {
-	run docker-compose down -v --remove-orphans
-	assert_success
-}
+testImpl() {
+    run echo "Starting test with arguments: $@"
 
-@test "Arrange - Build docker images [with PostgreSQL as DB]" {
-	cd $BATS_TEST_DIRNAME/..
-	run ${DOCKER_COMPOSE_WITH_POSTGRESQL_CMD_PREFIX} build
-	assert_success
-}
+    local -r docker_compose_options="$1"
+    if [ -n "${docker_compose_options}" ]; then
+        docker_compose_cmd_prefix="docker-compose ${docker_compose_options}"
+    else
+        docker_compose_cmd_prefix="docker-compose"
+    fi
+    run echo "docker_compose_cmd_prefix: ${docker_compose_cmd_prefix}"
 
-@test "Act - Start docker containers [with PostgreSQL as DB]" {
-	run ${DOCKER_COMPOSE_WITH_POSTGRESQL_CMD_PREFIX} up -d
-	assert_success
-}
+    run echo "Arrange - Build docker images"
 
-@test "Assert that docker containers are running [with PostgreSQL as DB]" {
-	run docker inspect -f {{.State.Running}} $OPBEANS_PHP_APP_CONTAINER_NAME
-	run docker inspect -f {{.State.Running}} $OPBEANS_PHP_WEB_CONTAINER_NAME
+    cd $BATS_TEST_DIRNAME/..
+    runCommand ${docker_compose_cmd_prefix} build
+
+    run echo "Act - Start docker containers"
+
+	runCommand ${docker_compose_cmd_prefix} up -d
+
+    run echo "Assert that docker containers are running"
+
+	runCommand docker inspect -f {{.State.Running}} ${OPBEANS_PHP_CONTAINER_NAME}
 	assert_output --partial 'true'
+	runCommand docker inspect -f {{.State.Running}} ${OPBEANS_PHP_FRONTEND_CONTAINER_NAME}
+	assert_output --partial 'true'
+
+    assertRunningWebServer "opbeans frontend" "http://127.0.0.1:${PORT}/"
+
+    assertRunningWebServer "opbeans-php (opbeans backend)" "http://opbeans-php:9000/"
+
+    run echo "Tear down - Stop docker containers"
+
+	runCommand ${docker_compose_cmd_prefix} down -v --remove-orphans
 }
 
-@test "Assert that opbeans app is running at port ${PORT} [with PostgreSQL as DB]" {
-	sleep 50
-	URL="http://127.0.0.1:${PORT}"
-	run curl -v --fail --connect-timeout 10 --max-time 30 "${URL}/"
-	assert_success
-	assert_output --partial 'HTTP/1.1 200'
+@test "Test with defaults" {
+    testImpl
 }
 
-@test "Tear down - Stop docker containers [with PostgreSQL as DB]" {
-	run ${DOCKER_COMPOSE_WITH_POSTGRESQL_CMD_PREFIX} down -v --remove-orphans
-	assert_success
+@test "Test with PostgreSQL as DB" {
+    testImpl "--env-file docker-compose_env_for_PostgreSQL.txt -f docker-compose_PostgreSQL.yml -f docker-compose.yml"
+}
+
+@test "Test with backend distributed tracing" {
+    testImpl "--env-file docker-compose_env_for_backend_distributed_tracing.txt -f docker-compose.yml -f docker-compose_backend_distributed_tracing.yml"
 }
