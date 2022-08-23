4233f784f1847e9db1817d847882a8ccb9f7d0ac
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2022-08-23 11:53:55 +0300

    Changed default port for the one container variant from 80 to 3000
    to align with other OpBeans-* implementations

--- docker_entrypoint_one_container.sh
+++ docker_entrypoint_one_container.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+cp /opt/docker/etc/nginx/vhost.conf /tmp/opt_docker_etc_nginx_vhost.conf_before_port_replace
+# Replace port in "listen 80 default_server;" and "listen [::]:80 default_server;"
+sed 's|listen 80 default_server;|listen 3000 default_server;|' /tmp/opt_docker_etc_nginx_vhost.conf_before_port_replace | \
+    sed 's|listen \[::\]:80 default_server;|listen [::]:3000 default_server;|' > /opt/docker/etc/nginx/vhost.conf
+
 php artisan migrate:fresh --seed
 
 /entrypoint "supervisord"
